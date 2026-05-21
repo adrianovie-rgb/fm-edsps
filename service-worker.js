47c24925d73b172684dfd604ec217c72a4eafc8f
adrianovie-rgb <adriano_vie@hotmail.com>
2026-05-21 01:36:10 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+var CACHE_NAME = 'fm-edsps-v1';
+var urlsToCache = [
+  './',
+  './index.html',
+  './dashboard.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.indexOf('script.google.com') !== -1) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(function(response) {
+        if (response.status === 200) {
+          var responseClone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      });
+    }).catch(function() {
+      return caches.match('./index.html');
+    })
+  );
+});
